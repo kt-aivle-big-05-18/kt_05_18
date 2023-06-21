27dfe5a9025797d420556a65e95d24f7a47a673d
hoomi <dds0410@naver.com>
2023-06-21 16:50:53 +0900

    정보 수정 가능

--- kt_coaching_base/mypage/urls.py
+++ kt_coaching_base/mypage/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('myp_info/', views.myp_info, name='myp_info'),
     path('myp_survey/', views.myp_survey, name='myp_survey'),
     path('myp_self/', views.myp_self, name='myp_self'),
+    path('update_profile/', views.update_profile, name='update_profile'),
 ]
--- kt_coaching_base/mypage/views.py
+++ kt_coaching_base/mypage/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def mypage_view(request):
     if not request.user.is_authenticated :
@@ -14,3 +15,27 @@ def myp_self(request):
 
 def myp_survey(request):
     return render(request, 'mypage/myp_survey.html')
+
+# @login_required
+def update_profile(request):
+    if request.method == 'POST':
+        user = request.user
+        
+        if 'update-department' in request.POST:
+            department = request.POST['department']
+            user.department = department            
+        elif 'update-rank' in request.POST:
+            rank = request.POST['rank']
+            user.rank = rank
+        elif 'update-password' in request.POST:
+            password = request.POST['password']
+            if len(password) > 0:
+                user.set_password(password)
+        
+        user.save()
+        messages.success(request, '프로필 정보가 성공적으로 업데이트되었습니다.')
+        return redirect('mypage:myp_info')
+    
+    else:
+        return render(request, 'mypage/myp_info.html')
+
